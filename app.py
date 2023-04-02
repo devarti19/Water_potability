3959480a1073380a026ec6ebe05c0f578985d157
Devarti Mahakalkar <50291981+devarti19@users.noreply.github.com>
2023-04-02 23:54:02 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = prediction[0]
+    if output:
+        o = ""
+    else:
+        o="not "
+
+    return render_template('predict.html', prediction_text='Water is $ {}potable.'.format(o))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
